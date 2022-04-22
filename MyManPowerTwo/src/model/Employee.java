85a23306792ac8209a3caeb2d8925e3ce5be5be6
Maneka611997 <manekakandy@gmail.com>
2022-04-22 11:39:20 +0530

    Commit 22.Read salary details from database.

--- MyManPowerTwo/src/model/Employee.java
+++ MyManPowerTwo/src/model/Employee.java
@@ -350,5 +350,62 @@ public class Employee {
 				return output; 
 				}
 				
+				
+				
+				public String readSalaryDetails()
+				{ 
+				 String output = ""; 
+				try
+				 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 { 
+				 return "Error while connecting to the database for reading."; 
+				 } 
+				 // Prepare the html table to be displayed
+				 output = "<table border='1'><tr><th>Email</th>" 
+				 +"<th>Month</th>"+"<th>Year</th>"
+				 + "<th>Amountr</th>" + "<th>Update</th><th>Remove</th></tr>"; 
+				 String query = "select * from salary"; 
+				 Statement stmt = con.createStatement(); 
+				 ResultSet rs = stmt.executeQuery(query); 
+				 // iterate through the rows in the result set
+				 while (rs.next()) 
+				 { 
+				 //String itemID = Integer.toString(rs.getInt("itemID")); 
+				 String a = rs.getString("email"); 
+				 String b = rs.getString("month"); 
+				 String c = rs.getString("year"); 
+				 String d = rs.getString("amount"); 
+				 
+				 //String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+				 
+				 // Add a row into the html table
+				 output += "<tr><td>" + a + "</td>"; 
+				 output += "<td>" + b + "</td>"; 
+				output += "<td>" + c + "</td>";
+				 
+				output += "<td>" +d + "</td>"; 
+				
+				 // buttons
+				 output += "<td><input name='btnUpdate' " 
+				 + " type='button' value='Update'></td>"
+				 + "<td><form method='post' action='indexSixteen.jsp'>"
+				 + "<input name='btnRemove' " 
+				 + " type='submit' value='Remove'>"
+				 + "<input name='itemID' type='hidden' " 
+				 + " value='" +001 + "'>"+ "</form></td></tr>"; 
+				 } 
+				 con.close(); 
+				 // Complete the html table
+				 output += "</table>"; 
+				 } 
+				catch (Exception e) 
+				 { 
+				 output = "Error while reading the items."; 
+				 System.err.println(e.getMessage()); 
+				 } 
+				return output; 
+				}
 
 }
